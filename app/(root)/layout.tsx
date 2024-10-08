8ab983591fc4abd583a50cb94ec1f6b0eb521b5d
Mayank Pant <142438135+pantboi2703@users.noreply.github.com>
2024-10-08 23:29:34 +0530

    Implement the complete layout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,12 +1,26 @@
+import Sidebar from "@/components/ui/Sidebar";
+import MobileNav from "@/components/ui/MobileNav";
+import Image from "next/image";
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const loggedIn = { firstName: 'Mayank', lastName: 'Pant'};
   return (
-    <main>
-        SIDEBAR
-        {children}
+    <main className="flex h-screen w-full font-inter">
+        <Sidebar user={loggedIn}/>
+
+        <div className="flex size-full flex-col">
+          <div className="root-layout">
+            <Image src="/icons/logo.svg" width={30} height={30} alt="logo"/>
+            <div>
+              <MobileNav user={loggedIn}/>
+            </div>
+          </div>
+          {children}
+        </div>
     </main>
   );
 }
---
+++ app/(root)/payment-transfer/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Transfer = () => {
+  return (
+    <div> transfer</div>
+  )
+}
+
+export default Transfer
